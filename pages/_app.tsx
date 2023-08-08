735ddaf8ac3841675029cf76d8252029efa22cba
juanrios456 <103297471+juanrios456@users.noreply.github.com>
2023-08-07 19:49:27 -0500

    Update to _app.tsx adding AOS
    AOS animation dependency that is required in order to make AOS work.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,21 @@ import { Layout } from '@/components/Layout'
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css'
+
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    AOS.init({
+      easing: 'ease-in',
+      once: false,
+      mirror: false,
+      offset: 100,
+      duration: 400,
+    });
+  }, []);
   return (
   <ChakraProvider>
     <Layout>
