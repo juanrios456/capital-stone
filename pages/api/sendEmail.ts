09aecde1ec7c281d739dedc2bb440b4b241bf285
juanrios456 <103297471+juanrios456@users.noreply.github.com>
2023-08-07 20:43:00 -0500

    Add files via upload

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -3,41 +3,26 @@ import { Resend } from 'resend'
 import { NextRequest, NextResponse } from "next/server";
 import WelcomeEmail from "../../email/welcome";
 
+
 const resend = new Resend(process.env.RESEND_API_KEY);
 const EMAIL_FROM = process.env.EMAIL_FROM;
 const EMAIL_TO = process.env.EMAIL_TO;
 
-export default async function handler(req:Request, res:NextApiResponse){
-  const data:any = await req.body
+export default async (req:NextApiRequest, res:NextApiResponse) =>{
+  const values:any =  await req.body
+
+  const parsedValues = JSON.parse(values);
 
     try {
-      await resend.sendEmail({
+      const data = await resend.emails.send({
         from: process.env.EMAIL_FROM || '',
         to: ['juanriis456@gmail.com'],
-        subject: `New Website Contact Form Recieved by:${data.name}`,
-        react: WelcomeEmail({
-          name: data.name,
-          phone: data.phone,
-          email: data.email,
-          details: data.details,
-          project: data.project,
-        }),
+        subject: `New Contact woops Form Recieved`,
+        react: WelcomeEmail( parsedValues ),
       });
 
-      return NextResponse.json({
-        status: 'Ok'
-      }, {
-        status: 200
-      })
-    } catch (e:unknown) {
-      if(e instanceof Error){
-        console.log(`Failed to Send Email: ${e.message}`)
-
-      }
-      return NextResponse.json({
-        error: 'Internal server error.'
-      }, {
-        status: 500
-      })
+      return res.status(200).json(data);
+    } catch (error) {
+      res.status(400).json(error);
     }
-  }
+  };
